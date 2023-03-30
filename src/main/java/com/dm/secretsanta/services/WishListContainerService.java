b5641d103ba3a9e8bc96ab38f4589959655b5f9c
Daniel Malicki <maldok31@gmail.com>
2023-03-30 15:24:48 +0200

    Added method to get wishes in price range

--- src/main/java/com/dm/secretsanta/services/WishListContainerService.java
+++ src/main/java/com/dm/secretsanta/services/WishListContainerService.java
@@ -4,7 +4,9 @@ import com.dm.secretsanta.entities.Wish;
 import com.dm.secretsanta.entities.WishListContainer;
 import com.dm.secretsanta.repositories.WishRepository;
 import jakarta.transaction.Transactional;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +26,14 @@ public class WishListContainerService {
   @Transactional
   public boolean addWishToList(WishListContainer wishListContainer, List<Wish> wishes) {
     return wishListContainer.getWishList().addAll(wishes);
+  }
+  
+  public List<Wish> getWishesInPriceRange(String email, BigDecimal lowerLimit, BigDecimal higherLimit){
+    var wishes = getWishListContainer(email).getWishList();
+
+    return wishes.stream()
+        .filter(wish -> wish.getPrice().compareTo(lowerLimit) >= 0)
+        .filter(wish -> wish.getPrice().compareTo(higherLimit) <= 0).toList();
   }
 
 }
